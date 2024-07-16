c9e7170415801d1d595d7db997075bd2cb6d145a
Dmitry_71 <dgorodilov@mail.ru>
2024-07-16 11:20:37 +0300

    initial commit

---
+++ src/main/java/ru/netology/SpringBoot1HwApplication.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot1HwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot1HwApplication.class, args);
+	}
+
+}
